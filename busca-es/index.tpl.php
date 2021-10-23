2a20d7c1c91f1d3e8fbdc6834f6d1e07acd147d8
rafaelcardosov <73447292+rafaelcardosov@users.noreply.github.com>
2021-10-23 19:05:11 -0300

    Add files via upload

--- busca-es/index.tpl.php
+++ busca-es/index.tpl.php
@@ -4,6 +4,10 @@
 	<?php include_once("includes/header.php"); ?>
 	<!-- HEADER -->
 
+	<!-- LIBRAS - INCLUSÃO -->
+	<?php include_once("includes/libras.php"); ?>
+	<!-- LIBRAS - INCLUSÃO -->
+	
 	<!-- BODY -->
 	<?php include_once("paginas/" . $secao . "/" . $secao . $sufixo . ".php"); ?>
 	<!-- BODY -->

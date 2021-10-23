7640981d6489331c2586dfb3d9040471566672eb
rafaelcardosov <73447292+rafaelcardosov@users.noreply.github.com>
2021-10-23 00:14:42 -0300

    Update database.class.php

--- busca-es/classes/database.class.php
+++ busca-es/classes/database.class.php
@@ -1,7 +1,28 @@
 <?php
 
 class database{
+	public $db = "mysql";
+    public $host = "localhost";
+    public $user = "root";
+    public $pass = "";
+    public $dbname = "busca_es";
+    public $port = 3308;
+    public $connect;
+    
+    public function connect() {
+        try{
+			$conn = new mysqli("".$this->host."","".$this->user."","".$this->pass."","".$this->dbname."");
 
+			if($conn){
+				return $conn;
+			}else{
+				die('Erro: Banco de Dados não conectou!');
+			}
+
+        } catch (Exception $ex) {
+            die('Erro: Banco de Dados fora do ar!');
+        }
+    }
 }
 
 ?>

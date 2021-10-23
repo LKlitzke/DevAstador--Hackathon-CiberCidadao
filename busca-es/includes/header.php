b40201cf43bcc2091a5c90e4cb6c43829d49b584
rafaelcardosov <73447292+rafaelcardosov@users.noreply.github.com>
2021-10-23 19:06:13 -0300

    Add files via upload

--- busca-es/includes/header.php
+++ busca-es/includes/header.php
@@ -1,51 +1,45 @@
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
-	<title>Busca ES</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <title>Busca ES</title>
 
-	<?php /* <link rel="stylesheet" href="<?php echo PATH; ?>/bootstrap/css/bootstrap.min.css"> */ ?>
-	<link rel="stylesheet" href="<?php echo PATH_CSS; ?>/header.css">
-	<?php /* <script src="<?php echo PATH; ?>/bootstrap/js/bootstrap.min.js"></script> */ ?>
+    <?php /* <link rel="stylesheet" href="<?php echo PATH; ?>/bootstrap/css/bootstrap.min.css"> */ ?>
+    <link rel="stylesheet" href="<?php echo PATH_CSS; ?>/header.css">
+    <?php /* <script src="<?php echo PATH; ?>/bootstrap/js/bootstrap.min.js"></script> */ ?>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-  	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  		<a class="navbar-brand" href="#">Navbar</a>
-  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-  			<span class="navbar-toggler-icon"></span>
-  		</button>
-
-  		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-  			<ul class="navbar-nav mr-auto">
-  				<li class="nav-item active">
-  					<a class="nav-link" href="<?php echo PATH; ?>">Pagina Inicial</a>
-  				</li>
-  				<li class="nav-item">
-  					<a class="nav-link" href="#">Link</a>
-  				</li>
-  				<li class="nav-item">
-  					<a class="nav-link disabled" href="#">Disabled</a>
-  				</li>
-  				<li class="nav-item dropdown">
-  					<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-  					<div class="dropdown-menu" aria-labelledby="dropdown01">
-  						<a class="dropdown-item" href="#">Action</a>
-  						<a class="dropdown-item" href="#">Another action</a>
-  						<a class="dropdown-item" href="#">Something else here</a>
-  					</div>
-  				</li>
-  			</ul>
-  			<form action="<?php echo PATH; ?>/busca/?send=1" class="form-inline my-2 my-lg-0">
-  				<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-  				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  			</form>
-  		</div>
-  	</nav>
-
-	<!-- <br clear="all"> -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo PATH; ?>">Pagina Inicial</a>
+                </li>
+                <?php if( empty($_SESSION['usuario_id']) ): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo PATH; ?>/login"><i class="fa fa-user-circle-o" aria-hidden="true"></i> Fazer login</a>
+                    </li>
+                <?php else: ?>
+                    <nav class="navbar navbar-light" style="font-size: 1rem;">
+                        <a class="navbar-brand" href="#">
+                            <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                            <?php echo $_SESSION['usuario_nome']; ?>
+                            <a href="<?php echo PATH; ?>/login/?logout=1">sair</a>
+                        </a>
+                    </nav>
+                <?php endif; ?>
+            </ul>
+            <?php // if($secao != 'home'): ?>
+                <form action="<?php echo PATH; ?>/busca/?send=1" method="POST" class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" name="palavra_chave" placeholder="Busca rápida" aria-label="Busca rápida">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+            <?php // endif; ?>
+        </div>
+    </nav>

65069836369bf7fe339a176857ea8c552c46a333
rafaelcardosov <73447292+rafaelcardosov@users.noreply.github.com>
2021-10-23 17:07:47 -0300

    Add files via upload

--- busca-es/paginas/busca/busca.php
+++ busca-es/paginas/busca/busca.php
@@ -1,27 +1,117 @@
 <?php
 
-$sql = "
-	SELECT 
-		id,
-		nome_setor,
-		conteudo,
-		DATE_FORMAT(data_cadastro, '%d/%m/%Y') AS data_cadastro,
-		DATE_FORMAT(data_atualizacao, '%d/%m/%Y') AS data_atualizacao
-	FROM 
-		setores 
-	WHERE 
-		nome_setor <> ''
-	ORDER BY
-		nome_setor ASC
-";
+function retorna_pagina($tabela){
+	if(empty($tabela)){
+		return '';
+	}
 
-$registros = array();
-if ($result = $mysqli->query($sql)) {
-    while($row = $result->fetch_assoc()){
-    	$registros[] = $row;
-    }
+	switch ($tabela) {
+		case 'documentos_requisitos': 
+			return 'documentos'; break;
+		case 'empresas_publicas': 
+			return 'empresas_publicas'; break;
+		case 'orgaos': 
+			return 'orgaos'; break;
+		case 'setores': 
+			return 'setores'; break;
+		
+		default:
+			return ''; break;
+	}
 }
 
-include_once("autarquia.tpl.php");
+$palavra_chave = addslashes($_POST['palavra_chave']);
+$resultados = [];
+
+$tabelas = [
+	'documentos_requisitos',
+	// 'empresas_publicas',
+	// 'orgaos',
+	'setores'
+];
+
+if(!empty($palavra_chave)){
+	$palavras_uteis = [];
+	
+	// Fazer separação de palavras e verificar se tem alguma palavra solta
+	$palavras = explode(' ', $palavra_chave);
+	if(!empty($palavras)){
+		foreach ($palavras as $palavra) {
+			if(!empty($palavra)){
+				$palavras_uteis[] = $palavra;
+			}
+		}
+	}
+
+	if(!empty($palavras_uteis)){
+		foreach ($palavras_uteis as $palavra) {
+			// Verificar o conteúdo dentro das tabelas
+			foreach ($tabelas as $tabela) {
+				if( strpos( $palavra, $tabela ) !== false ){
+					$resultados[] = [
+						'tipo' => 'tabela',
+						'nome' => '' . $tabela . '',
+						'pagina' => retorna_pagina($tabela),
+						'link' => PATH . '/' . retorna_pagina($tabela),
+						'conteudo' => 'Talvez essa página possa te ajudar!!'
+					];
+				}
+
+				$filtro = "";
+				$sql1 = "DESCRIBE {$tabela}";
+				if ($result = $mysqli->query($sql1)) {
+				    while($row = $result->fetch_assoc()){
+				    	$filtro .= " || " . $row['Field'] . " LIKE '%{$palavra}%' ";
+				    }
+				}
+
+				$sql2 = "SELECT * FROM {$tabela} WHERE 0 {$filtro} ";
+				if ($result2 = $mysqli->query($sql2)) {
+					// Algum resultado encontrado
+					$nome = "";
+					if($result2->num_rows > 0){
+						$sql3 = "SELECT nome_setor FROM setores WHERE nome_label = '".retorna_pagina($tabela)."' ";
+						if ($result3 = $mysqli->query($sql3)) {
+							while($row3 = $result3->fetch_assoc()){
+								$nome = $row3['nome_setor'];
+							}
+						}
+
+						while($row2 = $result2->fetch_assoc()){
+							$resultados[] = [
+								'tipo' => 'detalhes',
+								'nome' => '' . $tabela . '',
+								'pagina' => '' . $nome . '',
+								'id' => '' . $row2['id'] . '',
+								'link' => PATH . '/' . retorna_pagina($tabela) . '/' . $row2['id'] . '/' . urlencode($row2['nome']),
+								'conteudo' => 'Talvez o conteúdo dessa página possa te ajudar!!'
+							];
+						}
+					}
+				}
+			}
+		}
+
+	}else{
+		$resultados[0] = [
+			'status' => 'error',
+			'mensagem' => "Não foi digitada nenhuma palavra"
+		];
+	}
+}else{
+	$resultados[0] = [
+		'status' => 'error',
+		'mensagem' => "Não foi digitada nenhuma palavra"
+	];
+}
+
+if(empty($resultados)){
+	$resultados[0] = [
+		'status' => 'error',
+		'mensagem' => 'Nenhum resultado foi encontrado para sua busca!'
+	];
+}
+
+include_once("busca.tpl.php");
 
 ?>
--- busca-es/paginas/busca/busca.tpl.php
+++ busca-es/paginas/busca/busca.tpl.php
@@ -1,3 +1,36 @@
-<div class="container">
-	TESTE AUTARQUIA
-</div>
+<header class="py-5 bg-image-full" style="background-color: #a86f87;">
+	<div class="text-center my-5">
+		<img class="img-fluid rounded-circle mb-4" src="<?php echo PATH_PUBLIC; ?>/img/busca/busca_logo.png" alt="...">
+		<h1 class="text-white fs-3 fw-bolder">Resultados da sua busca</h1>
+	</div>
+</header>
+
+<section class="py-5">
+    <div class="container my-5">
+        <div class="">
+            <?php if(!empty($resultados[0]['status'])): ?>
+				<div class="row">
+					<div class="col-md-12">
+						<h5 class="card-title"><?php echo $resultados[0]['mensagem']; ?></h5>
+					</div>
+				</div>
+			<?php else: ?>
+				<?php foreach ($resultados as $row) { ?>
+					<?php if( $row['tipo'] == 'tabela' ): ?>
+						<div class="jumbotron">
+							<h1><?php echo $row['pagina']; ?></h1>
+							<p class="lead"><?php echo $row['conteudo']; ?></p>
+							<a class="btn btn-lg btn-primary" href="<?php echo $row['link']; ?>" role="button">Visualizar</a>
+						</div>
+					<?php elseif( $row['tipo'] == 'detalhes' ): ?>
+						<div class="jumbotron">
+							<h1><?php echo $row['pagina']; ?></h1>
+							<p class="lead"><?php echo $row['conteudo']; ?></p>
+							<a class="btn btn-lg btn-primary" href="<?php echo $row['link']; ?>" role="button">Visualizar</a>
+						</div>
+					<?php endif; ?>
+				<?php } ?>
+			<?php endif; ?>
+        </div>
+    </div>
+</section>

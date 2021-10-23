7102ee72e21999bda91254eec8a7a506f02c9976
LKlitzke <93014430+LKlitzke@users.noreply.github.com>
2021-10-23 20:35:09 -0300

    Update empresas detalhes

--- busca-es/paginas/empresas_publicas/empresas_publicas_detalhes.tpl.php
+++ busca-es/paginas/empresas_publicas/empresas_publicas_detalhes.tpl.php
@@ -6,14 +6,26 @@
 	</div>
 </header>
 
-<section class="py-5">
-    <div class="container my-5">
+<section class="py-2">
+    <div class="container my-2">
         <div class="row justify-content-center">
-            <div class="col-lg-6">
+            <div class="col-lg-10">
                 <!-- <h2>Full Width Backgrounds</h2> -->
-                <?php echo $info['conteudo']; ?>
-                <!-- <p class="lead">A single, lightweight helper class allows you to add engaging, full width background images to sections of your page.</p> -->
-                <!-- <p class="mb-0">The universe is almost 14 billion years old, and, wow! Life had no problem starting here on Earth! I think it would be inexcusably egocentric of us to suggest that we're alone in the universe.</p> -->
+                
+                <?php if(!empty($info['info1'])){ ?>
+                    <h4>O que é?</h4>
+                    <p style="text-align: justify; text-justify: inter-word;"><?php echo $info['info1'];?></p>
+                <?php } ?>
+                
+                <?php if(!empty($info['info2'])){ ?>
+                    <h4>Informações</h4>
+                    <p style="text-align: justify; text-justify: inter-word;"><?php echo $info['info2']; ?></p>
+                <?php } ?>
+
+                <?php if(!empty($info['link'])){ ?>
+                <h4>Redirecionamento</h4>
+                <a target="_blank" href="<?php echo $info['link'];?>">Site da <?php echo $info['nome'];?></a><br>
+                <?php } ?>
             </div>
         </div>
     </div>
